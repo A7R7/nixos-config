004a739cf3786e4cce1e512bdae8a3c5c7a6e60a
a7r7 <Aaron__Lee_@outlook.com>
2023-09-20 10:26:50 +0800

    add steam

--- home.nix
+++ home.nix
@@ -6,6 +6,15 @@
     homeDirectory = "/home/aaron-nix";
   };
 
+  nixpkgs = {
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = (_: true);
+    };
+  };
+
   xresources.properties = {
     "Xcursor.size" = 16;
     "Xft.dpi" = 172;
@@ -42,6 +51,7 @@
 
     firefox
     # vivaldi
+    steam
 
     eww-wayland
     cliphist
