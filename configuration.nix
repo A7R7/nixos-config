5a043f606487ce1d20ffd69f9a354e8cc9962534
a7r7 <Aaron__Lee_@outlook.com>
2023-09-25 15:34:39 +0800

    add hyprland

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [
       ./hardware-configuration.nix
+      ./hyprland.nix
     ];
   
   #bootloader
---
+++ hyprland.nix
@@ -0,0 +1,66 @@
+{ pkgs, ... }:
+{
+  services.xserver = {
+    displayManager.startx.enable = true;
+  };
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    # enableNvidiaPatches = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
+  };
+
+  security = {
+    polkit.enable = true;
+  };
+
+  environment.systemPackages = with pkgs.gnome; [
+    pkgs.glib
+    adwaita-icon-theme
+    nautilus
+    gnome-calendar
+    gnome-boxes
+    gnome-system-monitor
+    gnome-control-center
+    gnome-weather
+  ];
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
+  services = {
+    gvfs.enable = true;
+    devmon.enable = true;
+    udisks2.enable = true;
+    upower.enable = true;
+    accounts-daemon.enable = true;
+    gnome = {
+      evolution-data-server.enable = true;
+      glib-networking.enable = true;
+      gnome-keyring.enable = true;
+      gnome-online-accounts.enable = true;
+    };
+  };
+
+  hardware = {
+    bluetooth.enable = true;
+  };
+}
