b8c21671d933e478d556fae2db0a68e532239630
a7r7 <Aaron__Lee_@outlook.com>
2023-10-01 00:10:10 +0800

    add packages

--- home/home.nix
+++ home/home.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    ./packages.nix
+  ];
 
   home = {
     username = "aaron-nix";
@@ -32,102 +35,8 @@
   wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable = true;
+    extraConfig = ''
+      source=~/.config/hypr/main.conf
+    '';
   };
-
-  home.packages = with pkgs; [
-    neofetch
-    yq-go
-
-    kitty
-    alacritty
-
-    # clash-verge
-    # hyprland stuffs
-    hyprpaper
-    hyprpicker
-    wlogout
-    rofi-wayland-unwrapped
-
-    neovide
-    emacs29-pgtk
-
-    firefox
-    chromium
-    (vivaldi.override {
-      proprietaryCodecs = true;
-      enableWidevine = true;
-      commandLineArgs =
-       "--proxy-server='https=127.0.0.1:7890;http=127.0.0.1:7890'";
-    })
-    widevine-cdm
-    vivaldi-ffmpeg-codecs
-
-    steam-tui
-    steamcmd
-    steam
-
-    eww-wayland
-    gtk3
-    gtk-layer-shell
-    pango
-    gdk-pixbuf
-    cairo
-    glib
-    libgcc
-    glibc
-
-
-    cliphist
-    wl-clipboard
-
-    fira-code
-    fira-code-symbols
-    roboto
-    nerdfonts
-    sarasa-gothic 
-    
-    # tools
-    bat
-    eza
-    ranger nnn
-    fd
-    ripgrep
-    fzf
-    socat
-    jq
-    acpi
-    inotify-tools
-    ffmpeg
-    libnotify
-    zoxide
-
-    # tools
-    wl-gammactl
-    wl-clipboard
-    wf-recorder
-    hyprpicker
-    wayshot
-    swappy
-    slurp
-    imagemagick
-    pavucontrol
-    brightnessctl
-    swww
-
-
-    # langs
-    nodejs
-    go
-    bun
-    sassc
-    typescript
-    meson
-    ninja
-    # eslint
-  ];
-
-#  programs.clash-verge = {
-#    enable = true;
-#  };
-
 }
---
+++ home/packages.nix
@@ -0,0 +1,97 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    #guis
+    libreoffice
+    gimp
+    inkscape
+
+    neovide
+    emacs29-pgtk
+
+    firefox
+    chromium
+    (vivaldi.override {
+      proprietaryCodecs = true;
+      enableWidevine = true;
+      commandLineArgs =
+       "--proxy-server='https=127.0.0.1:7890;http=127.0.0.1:7890'";
+    })
+    steam-tui
+    steamcmd
+    steam
+
+    # clash-verge
+
+    # terminals & tuis
+    kitty
+    alacritty
+    wezterm
+
+    # tools
+    fastfetch
+    bat
+    eza
+    ranger nnn
+    fd
+    ripgrep
+    fzf
+    socat
+    jq
+    acpi
+    inotify-tools
+    ffmpeg
+    libnotify
+    zoxide
+
+    # hyprland
+    hyprpaper
+    hyprpicker
+    wlogout
+    rofi-wayland-unwrapped
+
+    # eww
+    eww-wayland
+    gtk3
+    gtk-layer-shell
+    pango
+    gdk-pixbuf
+    cairo
+    glib
+    libgcc
+    glibc
+
+
+    # fonts
+    fira-code
+    fira-code-symbols
+    roboto
+    nerdfonts
+    sarasa-gothic 
+    
+    # tools
+    wl-gammactl
+    wl-clipboard
+    wf-recorder
+    hyprpicker
+    wayshot
+    swappy
+    slurp
+    imagemagick
+    pavucontrol
+    brightnessctl
+    yq-go
+    swww
+
+
+    # langs
+    nodejs
+    go
+    bun
+    sassc
+    typescript
+    meson
+    ninja
+    # eslint
+  ];
+}
