bf217b65b26501b406ce5331d3b5422cf0b8c575
a7r7 <Aaron__Lee_@outlook.com>
2023-10-12 14:09:54 +0800

    change proxy address

--- proxy.sh
+++ proxy.sh
@@ -1,9 +1,14 @@
-sudo mkdir /run/systemd/system/nix-daemon.service.d/
-cat << EOF >/run/systemd/system/nix-daemon.service.d/override.conf  
+#!/usr/bin/env bash
+# this script restart nix-daemon with proxy settings as below
+
+path="/run/systemd/system/nix-daemon.service.d"
+content='
 [Service]
-Environment="http_proxy=socks5h://localhost:7891"
-Environment="https_proxy=socks5h://localhost:7891"
-Environment="all_proxy=socks5h://localhost:7891"
-EOF
+Environment="http_proxy=socks5h://localhost:7890"
+Environment="https_proxy=socks5h://localhost:7890"
+Environment="all_proxy=socks5h://localhost:7890"
+'
+sudo mkdir -p "$path"
+echo "$content" | sudo tee "$path/"override.conf
 sudo systemctl daemon-reload
 sudo systemctl restart nix-daemon
