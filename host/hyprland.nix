78954ed843409583c35bd9eec6513635ccf41b70
a7r7 <Aaron__Lee_@outlook.com>
2023-10-12 14:08:15 +0800

    hyprland temp fix

--- host/hyprland.nix
+++ host/hyprland.nix
@@ -6,15 +6,15 @@
 
   programs.hyprland = {
     enable = true;
+    # package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     xwayland.enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    # enableNvidiaPatches = true;
+    enableNvidiaPatches = true;
   };
 
   xdg.portal = {
     enable = true;
     wlr.enable = true;
-    # extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
+    extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
   };
 
   security = {
