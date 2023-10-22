4d378a2f770111d350ed8444b1ad5bb654d235b0
a7r7 <Aaron__Lee_@outlook.com>
2023-10-22 17:43:53 +0800

    fix $path not provided

--- proxy.sh
+++ proxy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # this script restart nix-daemon with proxy settings as below
 
 content='[Service]
@@ -6,7 +6,8 @@ Environment="http_proxy=socks5h://localhost:7890"
 Environment="https_proxy=socks5h://localhost:7890"
 Environment="all_proxy=socks5h://localhost:7890"
 '
-sudo mkdir -p /run/systemd/system/nix-daemon.service.d/
+path='/run/systemd/system/nix-daemon.service.d/'
+sudo mkdir -p "$path"
 echo "$content" | sudo tee "$path/"override.conf
 sudo systemctl daemon-reload
 sudo systemctl restart nix-daemon
