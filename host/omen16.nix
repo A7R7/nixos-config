d7499f89a6990d61f2233aaff49ee60da1fa7f51
a7r7 <Aaron__Lee_@outlook.com>
2024-04-25 13:48:04 +0800

    host =  home = 61

--- host/omen16.nix
+++ host/omen16.nix
@@ -1,5 +1,5 @@
 { config, lib, pkgs, ... }:
-let my_nvidia_x11 = config.boot.kernelPackages.nvidiaPackages.beta;
+let my_nvidia_x11 = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
 in {
   networking.hostName = "Omen16"; # Define your hostname.
 
