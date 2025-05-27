cddfbf3d42b9c4f238adcab329e13cbfd6b8023e
a7r7 <Aaron__Lee_@outlook.com>
2025-05-27 23:51:31 +0800

    host = 122 home = 87

---
+++ pkgs/vscode-custom/debug.js
@@ -0,0 +1,3 @@
+alert("!!! CUSTOM SCRIPT EXECUTED !!!"); // Very disruptive, but unmissable
+console.log("!!! MINIMAL CUSTOM SCRIPT LOG !!!");
+document.body.style.backgroundColor = "lime"; // Visually obvious change
